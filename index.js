de3cea10125f4086854206aeef9c7943d5b58225
guigs <evrat.guillaume@gmail.com>
2018-09-17 19:41:00 +0200

    Update subject code

--- index.js
+++ index.js
@@ -1,70 +1,67 @@
 const bitcoin = require('bitcoinjs-lib')
-const blocktrail = require('blocktrail-sdk')
-const client = blocktrail.BlocktrailSDK(
-  {
-    apiKey: '70b628e67e84d54aed84361f4990540cdfd29dca',
-    apiSecret: '62dcca80264ba4897ed2bb2f03a8922fb79b301c',
-    network: 'tBTC',
-    testnet: false
-  })
+const async = require('async')
 const BlockCypher = require('blockcypher')
+const cypherApi = new BlockCypher('btc', 'test3', '1ebe95fa2ef04389aa5434f5362ad3ec')
 
 // Connect to testnet network
 const testnet = bitcoin.networks.testnet
 
 // Import your bitcoin address from wif
-//const keyPair = bitcoin.ECPair.makeRandom({ network: testnet })
-const keyPair = bitcoin.ECPair.fromWIF('cPBCUQqvmuuz5q54JGeLhqik3APFMeK9iJFm9UPtZSmW8r7U4sUi', testnet);
-const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network: testnet })
+
+// Helper to print out result for balance and address informations
+function printResponse(err, data) {
+  if (err !== null) {
+    console.log(err)
+  } else {
+    console.log(data)
+  }
+}
+
+
+// Check out node modules library for cypher api
+// Get balance of your balance
+/* .... */
+
+// Get address informations
+/* .... */
+
+
+// Launch program to create and push tx
+createTransaction(address, 'msfQtj6q9wbDZV9RPHdWZSgh8q9r7rWXtU', function(unsignedtx) {
+  // Sign transaction ...
+  /* .... */
+
+  // Broadcast tx
+  /* .... */
+})
+
 
 // Function to get the input for the new transaction (txhash, index)
-function getInputNewTx (address) {
+function getInputNewTx (balance, address, callback) {
   /* To be defined...
    The function must return txhash of previous transaction and the index of the output
-   Must use client.addressUnspentOutputs to get unspent output */
-
+   Must use cypher address API endpoint to get unspent output */
 }
 
 // Function to create the transaction
-function createTransaction (addressFrom, addressTo, changeAddress) {
+function createTransaction (addressFrom, addressTo, callback) {
   /* To be defined...
-  1) Create an empty tx object
-  2) Add each output with address and amount
-  3) Add input with hash and index
-  4) Add change address without the fees
+  1) Get balance
+  2) Create an empty tx object on testnet
+  3) Add each output with address and amount
+  4) Add input with hash and index
+  5) Add change address without the fees
   */
-
-}
-
-// Function to send the transaction
-function signTx (wif, output) {
-  /* To be defined...
-  Use built in tx.sign function */
 }
 
 // Function to broadcast the transaction to the network
 function broadcastTx(txHex, callback) {
-  var cypherApi = new BlockCypher('btc', 'test3', '1ebe95fa2ef04389aa5434f5362ad3ec')
-  var pushTx = {
-    tx: txHex
-  }
-  cypherApi.pushTX(pushTx.tx, function (err, response) {
-    if (err) return callback(err)
-    callback(err, response)
-  })
+  // Use cypherApi.pushTX
 }
 
 // Function to get balance
-function getBalance (address) {
-    var amount = 0
-    client.addressUnspentOutputs(address, function (err, utxos) {
-      if (err) {
-        throw new Error(err)
-      }
-      utxos.data.forEach(function (item) {
-        //console.log(item.value)
-        amount += item.value
-      })
-      console.log('Bal ' + address + ' : ' + amount)
-    })
+function getBalance (address, callback) {
+    cypherApi.getAddr(address, { }, function(err, result) {
+      return callback(err, result.balance)
+    });
 }
